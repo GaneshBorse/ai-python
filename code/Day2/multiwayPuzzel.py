fd418d925ade9936b7bfeac736c22b6b61eeb2ce
Ganesh Borse <29756337+GaneshBorse@users.noreply.github.com>
2025-05-02 17:41:39 +0530

    puzzle

---
+++ code/Day2/multiwayPuzzel.py
@@ -0,0 +1,18 @@
+# WHich will never print regardless of the value of x? Consider any value of  x.
+# Q1
+if x < 2:
+    print("below 2")
+elif x >= 2:
+    print("2 / More")
+else:
+    print("Something else")
+
+# Q2
+if x < 2:
+    print("below 2")
+elif x < 20:
+    print("Below 20")
+elif x < 10:
+    print("Below 10")
+else:
+    print("Something else")
