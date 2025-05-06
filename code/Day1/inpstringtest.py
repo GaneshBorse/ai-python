b3ea1ceb3846da42284b19f479f5631728cc71f5
Ganesh Borse <29756337+GaneshBorse@users.noreply.github.com>
2025-05-06 22:39:06 +0530

    day 1

--- code/Day1/inpstringtest.py
+++ code/Day1/inpstringtest.py
@@ -1,4 +1,5 @@
 # usrinp = float(input("Anything "))
 usrinp = input("Anything ")
 # by default input takes everything as a STRING only, we may need to convert to ither data type eg. int,float
+
 print(type(usrinp), usrinp)
