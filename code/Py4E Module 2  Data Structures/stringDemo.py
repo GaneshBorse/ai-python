5021dd35ae2d163b92492eb88c1ce90f27804f2b
Ganesh Borse <29756337+GaneshBorse@users.noreply.github.com>
2025-09-02 13:39:10 +0530

    string ch 6

---
+++ code/Py4E Module 2  Data Structures/stringDemo.py
@@ -0,0 +1,25 @@
+strdemo = "Helllo Wassup!"
+# print(strdemo.find("H"))
+
+# new = strdemo.replace("Wassup", "Ganex")
+# print(new)
+
+# lstrip() for LeftEnd whitespace remover and rshift() for RightEnd whitespaces.
+# strip() removes from both end
+# str1 = "    Hekjwh ad fdeswF      "
+# print(str1.strip())
+
+# str.startswith() returns true false
+# print(strdemo.startswith("Helllo"))
+
+
+# str.find("word/char", index to start from)
+email = "From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008"
+atpos = email.find("@")
+print(atpos)
+
+spacepos = email.find(" ", atpos)
+print(spacepos)
+
+host = email[atpos + 1 : spacepos]
+print(host)
