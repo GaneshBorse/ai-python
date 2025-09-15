f582d3d6b1a9ed862962edee7dff3db5ff78a9f5
Ganesh Borse <29756337+GaneshBorse@users.noreply.github.com>
2025-09-15 21:46:08 +0530

    everything done

--- My code/Py4E Module 2  Data Structures/Assign6_5.py
+++ My code/Py4E Module 2  Data Structures/Assign6_5.py
@@ -2,13 +2,14 @@ text = "X-DSPAM-Confidence:    0.8475"
 col = text.find(":")
 # print(col)
 # print(len(text))
+# lsist = ["abc", "abvd", "sjdsd", "sd"]
 
 value = text[col + 1 : len(text)]
 # print(value)
 
 fvalue = value.strip()
 
-num=float(fvalue)
+num = float(fvalue)
 print(fvalue)
 """ 6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below.
  Convert the extracted value to a floating point number and print it out."""
